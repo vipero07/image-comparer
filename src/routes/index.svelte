9c69c61dc974835a75094a622a4dc9c457b151f8
Eitan Miller <yodaissmart@gmail.com>
2021-06-21 01:21:40 -0400

    mostly works

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,11 +5,31 @@
 	let rightImages = [];
 	let currentIndex = 0;
 	let verticalCompare = false;
+	let timeout = 5;
+	let timer;
 
 	$: initImage = leftImages[currentIndex];
 	$: imageTitle = leftImages.length !== 0 ? leftImages[currentIndex].name : '';
 	$: rightIndex = imageTitle !== '' ? rightImages.findIndex((x) => x.name === imageTitle) : 0;
 	$: compareImage = rightImages[rightIndex];
+	$: timeout >= 0 && leftImages.length && rightImages.length && setTimer();
+
+	const setTimer = () => {
+		if (timer) {
+			clearTimeout(timer);
+		}
+		if (timeout === 0) {
+			return;
+		}
+		timer = setTimeout(() => {
+			if (currentIndex < leftImages.length - 1) {
+				incrementIndex();
+			} else {
+				currentIndex = 0;
+			}
+			setTimer.call();
+		}, timeout * 1000);
+	};
 
 	const leftImagesInput = ({ detail: { files } }) => {
 		leftImages = [...leftImages, ...files];
@@ -19,52 +39,102 @@
 		rightImages = [...rightImages, ...files];
 	};
 
+	const handleKeydown = (event) => {
+		switch (event.keyCode) {
+			case 37:
+				// left arrow
+				decrementIndex();
+				break;
+			case 39:
+				incrementIndex();
+				// right arrow
+				break;
+			default:
+				break;
+		}
+	};
+
+	const incrementIndex = () => {
+		if (currentIndex < leftImages.length - 1) {
+			currentIndex = currentIndex + 1;
+		}
+	};
+
+	const decrementIndex = () => {
+		if (currentIndex > 0) {
+			currentIndex = currentIndex - 1;
+		}
+	};
 </script>
 
-<header>
-	<h1 class="text-xl">Pick images to compare</h1>
-	<div>
-		{#if currentIndex > 0}
-			<button on:click={() => (currentIndex = currentIndex - 1)}>Previous</button>
-		{/if}
-		{#if currentIndex < leftImages.length - 1}
-			<button on:click={() => (currentIndex = currentIndex + 1)}>Next</button>
-		{/if}
-	</div>
-</header>
+<svelte:window on:keydown={handleKeydown} />
 
-<main class="flex {verticalCompare ? 'flex-col' : 'flex-row'}">
-	<section class="w-1/2">
+<main class="h-full grid {verticalCompare ? 'grid-cols-1 grid-rows-2' : 'grid-cols-2 grid-rows-1'}">
+	<section>
 		<header>
-			<h3 class="text-lg">Left Images</h3>
+			<h3 class="text-lg text-center">Initial</h3>
 		</header>
 		<div>
 			<DropInput on:input={leftImagesInput}>
-				<div>
-					Drag &amp; Drop your file(s) or
-					<strong>browse.</strong>
+				<div class="text-center">
+					Drag &amp; Drop your image(s) or
+					<strong class="text-red-500 font-semibold">browse.</strong>
 				</div>
 				{#if leftImages.length !== 0}
-					<img src={URL.createObjectURL(initImage)} class="w-full h-full" alt="initial" />
+					<img
+						src={URL.createObjectURL(initImage)}
+						class="mx-auto max-w-full max-h-screen"
+						alt="initial"
+					/>
 				{/if}
 			</DropInput>
 		</div>
 	</section>
 
-	<section class="w-1/2">
+	<section>
 		<header>
-			<h3 class="text-lg">Right Images</h3>
+			<h3 class="text-lg text-center">Compare</h3>
 		</header>
 		<div>
 			<DropInput on:input={rightImagesInput}>
-				<div>
-					Drag &amp; Drop your file(s) or
-					<strong>browse.</strong>
+				<div class="text-center">
+					Drag &amp; Drop your images(s) or
+					<strong class="text-red-500 font-semibold">browse.</strong>
 				</div>
 				{#if rightImages.length !== 0 && rightIndex !== -1}
-					<img src={URL.createObjectURL(compareImage)} class="w-full h-full" alt="compared" />
+					<img
+						src={URL.createObjectURL(compareImage)}
+						class="mx-auto max-w-full max-h-screen"
+						alt="compared"
+					/>
 				{/if}
 			</DropInput>
 		</div>
 	</section>
 </main>
+
+<footer
+	class="grid grid-cols-3 place-content-center sticky inset-x-0 bottom-0 bg-white bg-opacity-70 h-12"
+>
+	{#if currentIndex > 0}
+		<div class="mx-auto">
+			<button
+				class="p-2 py-1 rounded shadow text-red-500 ring-red-200 bg-white focus:ring hover:ring"
+				on:click={decrementIndex}>Previous</button
+			>
+		</div>
+	{/if}
+	{#if leftImages.length || rightImages.length}
+		<div class="col-start-2 mx-auto">
+			Next Image Every <input bind:value={timeout} class="w-12" type="number" min="0" /> Seconds
+		</div>
+	{/if}
+	{#if currentIndex < leftImages.length - 1}
+		<div class="col-start-3 mx-auto">
+			<button
+				class="p-2 py-1 rounded shadow text-red-500 ring-red-200 bg-white focus:ring hover:ring"
+				on:click={incrementIndex}>Next</button
+			>
+		</div>
+	{/if}
+</footer>
--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -5,12 +6,18 @@ const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: [preprocess({
-        "postcss": true
-    })],
+		"postcss": true
+	})],
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		})
 	}
 };
 
